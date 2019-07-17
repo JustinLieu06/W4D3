181802250006ae77922c6e29858093cdaee9e7e2
JustinLieu06 <justinlieu06@gmail.com>
2019-07-17 16:22:35 -0700

    Working on rook/bishop/queen

--- Chess/rook_bishop_queen.rb
+++ Chess/rook_bishop_queen.rb
@@ -13,17 +13,38 @@ module Slideable
     DIAGONAL_DIRS
   end
 
+  def opp_color?(pos)
+    if self.color == :W
+      return true if board[pos.first][pos.last].color == :B
+    end
+
+    if self.color == :B
+      return true if board[pos.first][pos.last].color == :W
+    end
+    false
+  end
+
   def moves
     valid_moves = []
     cur_x, cur_y = self.pos
-    move_diffs.each do |(dx, dy)|
+    move_dirs.each do |(dx, dy)|
       new_pos = [cur_x + dx, cur_y + dy]
       if new_pos.all? { |coord| coord.between?(0, 7) }
         valid_moves << new_pos
       end
     end
 
-    valid_moves
+    full_moves = valid_moves
+    
+    valid_moves.each do |move|
+      step_move = move + move
+      if self.board[step_move].color == :N || self.board[step_move].opp_color?(move)
+        full_moves << step_move
+      end
+    end
+
+    full_moves
+
   end
 
   private
@@ -31,6 +52,7 @@ module Slideable
   end
 
   def grow_unblocked_moves_in_dir(dx, dy)
+    
   end
 
 end
